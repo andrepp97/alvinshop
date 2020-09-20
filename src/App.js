43ce989878378e1409d9adb4664c23e719a217e5
Andre Putera Pratama <andreputerap@gmail.com>
2020-09-21 02:44:55 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,60 +1,49 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { useContext, useEffect } from 'react';
+import { AuthContext } from './context/AuthContext';
 
 // COMPONENTS
+import Router from './Routes/Router';
 import Navbar from './components/Header';
 import Sidebar from './components/Sidebar';
+import LoadingScreen from './components/LoadingScreen';
 
-// PAGES
-import Auth from './pages/Auth';
-import Dashboard from './pages/Dashboard';
-import Products from './pages/Products';
-import Category from './pages/Category';
-import Orders from './pages/Orders';
-import Banners from './pages/Banners';
-
-
-class App extends Component {
-  state = {
-    currentURL: ''
-  }
+const App = () => {
+  // CONTEXT
+  const { userState, dispatch } = useContext(AuthContext)
 
   // LIFECYCLE
-  componentDidMount() {
-    let url = window.location.pathname
-    this.setState({ currentURL: url })
-  }
+  useEffect(() => {
+    const restoreToken = () => {
+      let userToken = localStorage.getItem("@alvinshop")
+      setTimeout(() => {
+        dispatch({
+          type: "LOGIN",
+          id: 1,
+          username: "Alvinshop",
+          role: "sysadmin",
+          token: userToken,
+        })
+      }, 1000)
+    }
 
-  // MAIN RENDER
-  render() {
-    // Destructure
-    const { currentURL } = this.state
+    restoreToken()
+  }, [dispatch])
 
-    return (
-      <>
-        { currentURL === '/' ? null : <Navbar /> }
+  // RENDER
+  return userState.isLoading
+  ? <LoadingScreen />
+  : (
+    <div>
+        { userState.userToken ? <Navbar /> : null}
         
-        { currentURL === '/' ? null : <Sidebar /> }
+        { userState.userToken ? <Sidebar /> : null}
 
-        <div
-          id="main-content"
-          className="py-3 px-2"
-          style={ currentURL === '/' ? null : { marginLeft:'225px', transition:'.25s' } }
-        >
+        <main id="main-content" className={userState.userToken ? "content-shift" : null}>
+          <Router />
+        </main>
+    </div>
+  );
+};
 
-          <Switch>
-            <Route exact path='/' component={Auth} />
-            <Route exact path='/Dashboard' component={Dashboard} />
-            <Route exact path='/Products' component={Products} />
-            <Route exact path='/Category' component={Category} />
-            <Route exact path='/Orders' component={Orders} />
-            <Route exact path='/Banners' component={Banners} />
-          </Switch>
-
-        </div>
-      </>
-    );
-  }
-}
 
 export default App;

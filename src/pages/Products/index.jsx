2982ade46d57c827308edc208f61389b76f0babb
Andre Putera Pratama <andreputerap@gmail.com>
2020-12-19 18:49:24 +0700

    Update Products page

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,119 +1,144 @@
-import React, { Component } from 'react';
-import NumberFormat from 'react-number-format';
-import { MDBDataTable, MDBTable, MDBTableBody, MDBTableHead } from 'mdbreact';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import Pagination from '@material-ui/lab/Pagination';
+import IconButton from '@material-ui/core/IconButton';
+import InputBase from '@material-ui/core/InputBase';
+import Tooltip from '@material-ui/core/Tooltip';
+import { Search, Description } from '@material-ui/icons';
 
-const myData = {
-    columns: [
-        {
-            label: '#',
-            field: 'id',
-            sort: 'asc',
-            width: 100
-          },
-          {
-            label: 'Name',
-            field: 'name',
-            sort: 'asc',
-            width: 10
-          },
-          {
-            label: 'Price',
-            field: 'price',
-            sort: 'asc',
-            width: 200
-          },
-          {
-            label: 'Stock',
-            field: 'stock',
-            sort: 'asc',
-            width: 100
-          }
-    ],
-    rows: [
-        { id: 1, name: 'Final Fantasy VII Remake', price: <NumberFormat value={1199000} displayType={'text'} thousandSeparator/>, stock: 20 },
-        { id: 2, name: 'Dead By Deadlight', price: <NumberFormat value={499000} displayType={'text'} thousandSeparator/>, stock: 4 },
-        { id: 3, name: 'Monster Hunter World', price: <NumberFormat value={699000} displayType={'text'} thousandSeparator/>, stock: 12 },
-        { id: 4, name: 'Warriors Orochi 4', price: <NumberFormat value={375000} displayType={'text'} thousandSeparator/>, stock: 10 },
-    ]
-}
+// OTHER IMPORTS
+import APIRequest from '../../api/APIRequest';
+import Loader from '../../components/Loader';
+import { useStyles } from '../../assets/styles/GlobalStyle';
+import { productColumns } from '../../helper/StaticData';
 
-class Products extends Component {
-    state = {
-        columns: ['#', 'Name', 'Price', 'Stock'],
-        data: [
-            { id: 1, name: 'Final Fantasy VII Remake', price: 1200000, stock: 20 },
-            { id: 2, name: 'Dead By Deadlight', price: 499000, stock: 4 },
-            { id: 3, name: 'Monster Hunter World', price: 750000, stock: 12 },
-            { id: 4, name: 'Warriors Orochi 4', price: 499000, stock: 10 },
-        ]
-    }
+
+const ProductPage = () => {
+    // VARIABLES
+    const limit = 10
+    const classes = useStyles()
+
+    // STATE
+    const [total, setTotal] = useState(0)
+    const [pagination, setPagination] = useState(1)
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
 
     // LIFECYCLE
-    componentDidMount() {
-        document.title = "ALVIN SHOP | Products"
-    }
-    
-    // MAIN RENDER
-    render() {
-        return (
-            <div className="container-fluid">
+    useEffect(() => {
+        const getProdukData = () => {
+            setLoading(true)
+            const body = { limit, offset: pagination }
+            APIRequest.post('admin/showProduk', body)
+            .then(({data}) => {
+                console.log("Products", data)
+                // setProducts(data.data.data)
+                // if (data.data.total) setTotal(data.data.total)
+            })
+            .catch(err => console.log(err))
+            .finally(() => setLoading(false))
+        }
 
-                <div className="page-title">
-                    <h2>Products</h2>
-                </div>
+        getProdukData()
+    }, [pagination])
 
-                <div className="card">
-                    <div className="card-body">
-                        <MDBTable hover responsive>
-                            <MDBTableHead>
-                                <tr>
-                                    {this.state.columns.map((column,idx) => (
-                                        <td key={idx} className={`font-weight-bold opacity-70 ${column === '#' ? 'text-left' : 'text-center'}`}>
-                                            {column}
-                                        </td>
-                                    ))}
-                                </tr>
-                            </MDBTableHead>
-                            <MDBTableBody>
-                                {this.state.data.map((item,idx) => (
-                                    <tr key={idx}>
-                                        <td className="text-left">
-                                            {item.id}
-                                        </td>
-                                        <td className="text-left" style={{ minWidth:'7rem' }}>
-                                            {item.name}
-                                        </td>
-                                        <td className="text-center">
-                                            <NumberFormat
-                                                prefix={'Rp '}
-                                                value={item.price}
-                                                displayType={'text'}
-                                                thousandSeparator={true}
-                                            />
-                                        </td>
-                                        <td className='text-center'>
-                                            {item.stock}
-                                        </td>
-                                    </tr>
-                                ))}
-                            </MDBTableBody>
-                        </MDBTable>
-                    </div>
-                </div>
-
-                <div className="card mt-5">
-                    <div className="card-body">
-                        <MDBDataTable
-                            small
-                            bordered
-                            data={myData}
+    // RENDER
+    return (
+        <div>
+            
+            <div className={classes.titleBar}>
+                <h2 className={classes.opacity70}>
+                    Products
+                </h2>
+                <div className={classes.flexRow}>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <Search />
+                        </div>
+                        <InputBase
+                            placeholder="Search…"
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                            inputProps={{ 'aria-label': 'search' }}
                         />
                     </div>
                 </div>
-
             </div>
-        );
-    }
-}
 
-export default Products;
+            <TableContainer className={classes.tableContainer}>
+                {
+                    loading
+                    ? <Loader />
+                    : <Table stickyHeader>
+                        <TableHead>
+                            <TableRow>
+                                {productColumns.map(column => (
+                                    <TableCell
+                                        size="small"
+                                        key={column.id}
+                                        align={column.align}
+                                        style={{ minWidth: column.minWidth, fontWeight: 'bold', color: '#4c4c4c' }}
+                                    >
+                                        {column.label}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {products ? products.map((row,idx) => (
+                                <TableRow hover tabIndex={-1} key={idx}>
+                                    <TableCell>
+                                        {(pagination-1) * limit + (idx+1)}
+                                    </TableCell>
+                                    <TableCell>
+                                        {row.title}
+                                    </TableCell>
+                                    <TableCell>
+                                        {row.stock}
+                                    </TableCell>
+                                    <TableCell>
+                                        {row.price}
+                                    </TableCell>
+                                    <TableCell>
+                                        {row.discount}
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        <Tooltip className={classes.mx1} title="Detail">
+                                            <Link to={`/product/${row.id}`}>
+                                                <IconButton size="small">
+                                                    <Description fontSize="small" color="primary" />
+                                                </IconButton>
+                                            </Link>
+                                        </Tooltip>
+                                    </TableCell>
+                                </TableRow>
+                            )) : null}
+                        </TableBody>
+                    </Table>
+                }
+            </TableContainer>
+
+            <Pagination
+                size="small"
+                color="primary"
+                showLastButton
+                showFirstButton
+                page={pagination}
+                count={Math.ceil(total/limit)}
+                onChange={(event, page) => setPagination(page)}
+            />
+            
+        </div>
+    );
+};
+
+export default ProductPage;

2d022e13cb5f678d899c0a097c4721abaa4ee366
Andre Putera Pratama <andreputerap@gmail.com>
2020-12-19 18:49:04 +0700

    Create StaticData.js

---
+++ src/helper/StaticData.js
@@ -0,0 +1,34 @@
+export const productColumns = [
+    {
+        id: 1,
+        label: '#',
+        align: 'left',
+    },
+    {
+        id: 2,
+        label: 'Title',
+        minWidth: 150,
+        align: 'left',
+    },
+    {
+        id: 3,
+        label: 'Stock',
+        align: 'right',
+    },
+    {
+        id: 4,
+        label: 'Price',
+        align: 'right',
+    },
+    {
+        id: 5,
+        label: 'Discount',
+        align: 'right',
+    },
+    {
+        id: 6,
+        label: 'Action',
+        minWidth: 100,
+        align: 'center',
+    },
+]

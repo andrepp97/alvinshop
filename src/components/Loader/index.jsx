06d7385b7f5b39bdaa5a77565765eeeb2182674c
Andre Putera Pratama <andreputerap@gmail.com>
2020-12-19 18:48:55 +0700

    Create Loader

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+}))
+
+const Loader = () => {
+    const classes = useStyles()
+    return (
+        <div className={classes.root}>
+            <CircularProgress />
+        </div>
+    );
+};
+
+export default Loader;

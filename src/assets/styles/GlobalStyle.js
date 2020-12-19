b8a6b2a922a3caac5f814e8fd84c8072085451ee
Andre Putera Pratama <andreputerap@gmail.com>
2020-12-19 18:48:39 +0700

    Create GlobalStyle.js

---
+++ src/assets/styles/GlobalStyle.js
@@ -0,0 +1,85 @@
+import { makeStyles, fade } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles((theme) => ({
+    root: {
+        paddingRight: theme.spacing(4),
+        paddingLeft: theme.spacing(4),
+        paddingTop: theme.spacing(5),
+        paddingBottom: theme.spacing(1),
+    },
+    tableContainer: {
+        height: 'calc(100vh - 186px)',
+        marginBottom: theme.spacing(2),
+        overflow: 'auto',
+    },
+    titleBar: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: '10px'
+    },
+    flexRow: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade('#f2f2f2', 0.5),
+        '&:hover': {
+          backgroundColor: fade('#f2f2f2', 0.75),
+        },
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          marginLeft: theme.spacing(1),
+          width: 'auto',
+        },
+      },
+      searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      inputRoot: {
+        color: 'inherit',
+      },
+      inputInput: {
+        padding: '11px 0',
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          width: '12ch',
+          '&:focus': {
+            width: '20ch',
+          },
+        },
+    },
+    tableImage: {
+      width: '40px',
+      height: '40px',
+      objectFit: 'cover',
+      objectPosition: 'center',
+      borderRadius: '4px',
+    },
+    opacity70: {
+        opacity: .7
+    },
+    my1: {
+        marginTop: '8px',
+        marginBottom: '8px',
+    },
+    mx1: {
+        marginLeft: '4px',
+        marginRight: '4px',
+    },
+    py1: {
+        paddingTop: '8px',
+        paddingBottom: '8px',
+    }
+}))

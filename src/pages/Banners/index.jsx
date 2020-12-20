587370be2a8d86780ea96a017667d2eac9e2f6bd
Andre Putera Pratama <andreputerap@gmail.com>
2020-12-20 14:00:20 +0700

    Edit & Delete banner finished

--- src/pages/Banners/index.jsx
+++ src/pages/Banners/index.jsx
@@ -12,6 +12,7 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 // OTHER IMPORTS
 import APIRequest from '../../api/APIRequest';
 import Loader from '../../components/Loader';
+import ActionDialog from '../../components/ActionDialog';
 import { useStyles } from '../../assets/styles/GlobalStyle';
 
 // FILEPOND
@@ -34,6 +35,26 @@ const BannerPage = () => {
     const [newBanner, setNewBanner] = useState([])
     const [bannerName, setBannerName] = useState('')
     const [bannerURL, setBannerURL] = useState('')
+    const [selected, setSelected] = useState(null)
+    const [editMode, setEditMode] = useState(false)
+
+    // EDIT STATE
+    const [editBanner, setEditBanner] = useState([])
+    const [editName, setEditName] = useState('')
+    const [editURL, setEditURL] = useState('')
+    
+    // DIALOG STATE
+    const [open, setOpen] = useState(false)
+    const [dialogText, setDialogText] = useState('')
+
+    // HANDLE DIALOG
+    const handleOpen = () => {
+        setOpen(true)
+    };
+    
+    const handleClose = () => {
+        setOpen(false)
+    };
 
     // VALIDATION
     const validateUpload = () => {
@@ -71,12 +92,49 @@ const BannerPage = () => {
             formdata.append('url', bannerURL)
 
             APIRequest.post('admin/addBanner', formdata)
-            .then(() => getBannerData())
+            .then(() => {
+                getBannerData()
+                setNewBanner([])
+                setBannerURL('')
+                setBannerName('')
+            })
             .catch(err => {
                 console.log(err.response)
                 setLoading(false)
             })
         }
+    }
+
+    const onEditBanner = () => {
+        setLoading(true)
+
+        var formdata = new FormData()
+        formdata.append('image', editBanner.length ? editBanner[0].file : null)
+        formdata.append('banner_name', editName)
+        formdata.append('url', editURL)
+        formdata.append('id', selected)
+
+        APIRequest.put('admin/editBanner', formdata)
+        .then(() => {
+            getBannerData()
+            setEditMode(false)
+        })
+        .catch(err => {
+            console.log(err.response)
+            setLoading(false)
+        })
+    }
+
+    const onDeleteBanner = () => {
+        setLoading(true)
+        const body = { id: selected }
+
+        APIRequest.post('admin/deleteBanner', body)
+        .then(() => getBannerData())
+        .catch(err => {
+            console.log(err.response)
+            setLoading(false)
+        })
     }
 
     // LIFECYCLE
@@ -98,39 +156,116 @@ const BannerPage = () => {
 
             <Grid container spacing={3}>
 
-                {banners.length ? banners.map(banner => (
-                    <Grid
-                        item
-                        xs={12}
-                        sm={6}
-                        md={4}
-                        lg={3}
-                        key={banner.id}
-                    >
-                        <Card>
-                            <CardActionArea>
-                                <CardMedia
-                                    className={classes.media}
-                                    title={banner.banner_name}
-                                    image={baseURL + prefix + '/' + banner.image}
+                {banners.length ? banners.map(banner => {
+                    if (editMode && selected === banner.id) {
+                        return (
+                            <Grid
+                                item
+                                xs={12}
+                                sm={6}
+                                md={4}
+                                lg={3}
+                                key={banner.id}
+                            >
+                                <FilePond
+                                    maxFiles={1}
+                                    files={editBanner}
+                                    allowMultiple={false}
+                                    acceptedFileTypes={['image/*']}
+                                    onupdatefiles={files => setEditBanner(files)}
+                                    labelIdle='<span class="filepond--label-action">Drag & Drop your Banner here</span>'
                                 />
-                                <CardContent>
-                                    <Typography variant="subtitle1" component="span">
-                                        {banner.banner_name}
-                                    </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                            <CardActions>
-                                <Button size="small" color="primary">
-                                    Edit
-                                </Button>
-                                <Button size="small" color="secondary">
-                                    Delete
-                                </Button>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                )) : null}
+                                <TextField
+                                    size="small"
+                                    variant="outlined"
+                                    label="Banner Name"
+                                    value={editName}
+                                    className={classes.textInput}
+                                    onChange={e => setEditName(e.target.value)}
+                                />
+                                <TextField
+                                    size="small"
+                                    variant="outlined"
+                                    label="Banner URL"
+                                    value={editURL}
+                                    className={classes.textInput}
+                                    onChange={e => setEditURL(e.target.value)}
+                                />
+                                <div className={classes.textRight}>
+                                    <Button
+                                        size="small"
+                                        onClick={() => {
+                                            setEditMode(false)
+                                            setSelected(null)
+                                        }}
+                                    >
+                                        Cancel
+                                    </Button>
+                                    <span>&nbsp;</span>
+                                    <Button
+                                        size="small"
+                                        color="primary"
+                                        variant="contained"
+                                        onClick={onEditBanner}
+                                    >
+                                        Save
+                                    </Button>
+                                </div>
+                            </Grid>
+                        )
+                    } else {
+                        return (
+                            <Grid
+                                item
+                                xs={12}
+                                sm={6}
+                                md={4}
+                                lg={3}
+                                key={banner.id}
+                            >
+                                <Card>
+                                    <CardActionArea>
+                                        <CardMedia
+                                            className={classes.media}
+                                            title={banner.banner_name}
+                                            image={baseURL + prefix + '/' + banner.image}
+                                        />
+                                        <CardContent>
+                                            <Typography variant="subtitle1" component="span">
+                                                {banner.banner_name}
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                    <CardActions>
+                                        <Button
+                                            size="small"
+                                            color="primary"
+                                            onClick={() => {
+                                                setEditMode(true)
+                                                setSelected(banner.id)
+                                                setEditName(banner.banner_name)
+                                                setEditURL(banner.url)
+                                            }}
+                                        >
+                                            Edit
+                                        </Button>
+                                        <Button
+                                            size="small"
+                                            color="secondary"
+                                            onClick={() => {
+                                                setDialogText(<span>Delete <strong>{banner.banner_name}</strong> ?</span>)
+                                                setSelected(banner.id)
+                                                handleOpen()
+                                            }}
+                                        >
+                                            Delete
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        )
+                    }
+                }) : null}
 
                 <Grid
                     item
@@ -141,7 +276,7 @@ const BannerPage = () => {
                 >
                     <FilePond
                         maxFiles={1}
-                        newBanner={newBanner}
+                        files={newBanner}
                         allowMultiple={false}
                         acceptedFileTypes={['image/*']}
                         onupdatefiles={files => setNewBanner(files)}
@@ -176,6 +311,14 @@ const BannerPage = () => {
                 </Grid>
 
             </Grid>
+
+            <ActionDialog
+                isOpen={open}
+                handleClose={handleClose}
+                dialogText={dialogText}
+                action={onDeleteBanner}
+            />
+
         </div>
     );
 };
